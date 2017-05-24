c860c00227f105c841ac4f9aa6efb0c3493ba215
jmle <juanmanuel.estrada@sky.uk>
2017-05-24 14:47:38 +0100

    Remove logging and debugging

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-debugger;
-console.log("hey");
-
 var hasProfessionalRatings = document.getElementsByClassName("wikitable infobox")[0].innerHTML.includes("Professional ratings");
 
 if (hasProfessionalRatings) {
-    console.log("gramenauer");
-
     var professionalRatingsTable = document.getElementsByClassName("wikitable infobox")[0];
     professionalRatingsTable.parentElement.removeChild(professionalRatingsTable);
 }
