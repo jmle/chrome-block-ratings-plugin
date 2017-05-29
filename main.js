48fe82b483ea36e3cf61c68e9d289ca7fb6b1976
jmle <ypsilon888@gmail.com>
2017-05-29 12:31:46 +0100

    Refactor code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var hasProfessionalRatings = document.getElementsByClassName("wikitable infobox")[0].innerHTML.includes("Professional ratings");
+var professionalRatingsTable = document.getElementsByClassName("wikitable infobox")[0];
 
+var hasProfessionalRatings = professionalRatingsTable.innerHTML.includes("Professional ratings");
 if (hasProfessionalRatings) {
-    var professionalRatingsTable = document.getElementsByClassName("wikitable infobox")[0];
     professionalRatingsTable.parentElement.removeChild(professionalRatingsTable);
 }
